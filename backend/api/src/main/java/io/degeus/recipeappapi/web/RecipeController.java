cbf341fb53b8fe33977e586f775f2000e72e2c3d
wouter <wouter@degeus.io>
2021-08-22 20:45:42 +0200

    TDD 2/3: fixing testautomation test for swagger documentation.

--- backend/api/src/main/java/io/degeus/recipeappapi/web/RecipeController.java
+++ backend/api/src/main/java/io/degeus/recipeappapi/web/RecipeController.java
@@ -3,6 +3,7 @@ package io.degeus.recipeappapi.web;
 import io.degeus.recipeappapi.config.PersistenceConfig;
 import io.degeus.recipeappapi.domain.Recipe;
 import io.degeus.recipeappapi.service.RecipeService;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -12,10 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 import java.util.UUID;
 
+@Api(tags = "vat",
+        description = " " //overrides default name of 'Health Controller' if left empty
+)
 @RestController
 @RequestMapping(path = RecipeController.REQUEST_MAPPING)
 @RequiredArgsConstructor
@@ -26,9 +31,11 @@ public class RecipeController {
     public static final String REQUEST_MAPPING = "/recipes";
     private final RecipeService recipeService;
 
+    @ApiOperation("Create a Recipe")
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Created")})
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Recipe> create(@RequestBody Recipe candidate,
-                                         UriComponentsBuilder ucb) {
+    public ResponseEntity<Recipe> create(@ApiParam("The candidate Recipe") @RequestBody Recipe candidate,
+                                         @ApiIgnore @ApiParam(hidden = true) UriComponentsBuilder ucb) {
 
         Recipe created = recipeService.create(candidate);
         UriComponents uriComponents = ucb.path(REQUEST_MAPPING + "/{id}")
@@ -36,25 +43,36 @@ public class RecipeController {
         return ResponseEntity.created(uriComponents.toUri()).build();
     }
 
+    @ApiOperation("Get a Recipe")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = Recipe.class)})
     @GetMapping(value = "/{recipe_id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Recipe get(@PathVariable("recipe_id") String recipeId) {
+    public Recipe get(
+            @ApiParam("The Recipe's id") @PathVariable("recipe_id") String recipeId) {
         return recipeService.findById(UUID.fromString(recipeId));
     }
 
+    @ApiOperation("List all Recipes")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = Recipe[].class)})
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Recipe> getAll() {
         return recipeService.findAll();
     }
 
+    @ApiOperation("Update a Recipe")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = Recipe.class)})
     @PutMapping(value = "/{recipe_id}",
             consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Recipe update(@PathVariable("recipe_id") String recipeId,
-                         @RequestBody Recipe candidate) {
+    public Recipe update(
+            @ApiParam("The Recipe's id") @PathVariable("recipe_id") String recipeId,
+            @ApiParam("The Recipe's new contents") @RequestBody Recipe candidate) {
         return recipeService.update(UUID.fromString(recipeId), candidate);
     }
 
+    @ApiOperation("Delete a Recipe")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = Void.class)})
     @DeleteMapping(value = "/{recipe_id}")
-    public ResponseEntity delete(@PathVariable("recipe_id") String recipeId) {
+    public ResponseEntity delete(
+            @ApiParam("The Recipe's id") @PathVariable("recipe_id") String recipeId) {
         recipeService.delete(UUID.fromString(recipeId));
         return ResponseEntity.ok().build();
     }
